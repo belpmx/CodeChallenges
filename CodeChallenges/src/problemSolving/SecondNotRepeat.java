b3ddc654233e43c37ff9a02e342e97143b532050
oem <iscpabel@gmail.com>
2025-06-11 06:23:37 -0600

    Add: Second not repeated.

--- CodeChallenges/src/problemSolving/SecondNotRepeat.java
+++ CodeChallenges/src/problemSolving/SecondNotRepeat.java
@@ -1,6 +1,25 @@
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class SecondNotRepeat {
 
     public static char findSecondNonRepeated(String value){
+        //List<Character> countChars = value
+        // Build a frequency map of characters while preserving the order of appearance
+        Map <Character, Long> freq = value.chars().mapToObj(c-> (char)c)
+                .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()));
+        // Using stream to filter out repeated characters,
+        // skip the first non-repeated and take the second one.
+        return freq.entrySet().stream()
+                .filter(entry -> entry.getValue() == 1) // non-repeated characters only
+                .skip(1) // skip the first non-repeated character
+                .findFirst() // attempt to get the second one
+                .map(Map.Entry::getKey)
+                .orElse('0'); // return '0' if not present
+/*
         int [] countChars = new int[26];
         for (char thisChar: value.toCharArray()){
             countChars[thisChar -'a']++;
@@ -12,7 +31,7 @@ public class SecondNotRepeat {
             if(countUnique ==2)
                 return thisChar;
         }
-        return '0';
+        return '0';*/
     }
 
     public static void main(String[] args) {
